c12c0677913754cd2bbb12563f4efd6174bb4436
Zarb <alanzarb@users.noreply.github.com>
2020-02-13 23:52:06 -0500

    m6

--- selen.py
+++ selen.py
@@ -6,7 +6,8 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Edge()
+driver = webdriver.Edge(
+    "C:\\Users\\alanw\\AppData\\Local\\Programs\\Python\\Python38\\MicrosoftWebDriver.exe")
 driver.get("https://instagram.com")
 
 sleep(2)
