71c7763705f1d44d8120e80b4640ad02b3cd6432
Zarb <alanzarb@users.noreply.github.com>
2020-02-18 00:15:48 -0500

    b0

---
+++ bobclass.py
@@ -0,0 +1,35 @@
+# bob.py Class with Class variables
+
+
+class BobClass:
+    ''' population is a class variable '''
+
+    population = 0
+
+    def __init__(self, name):
+        BobClass.population = BobClass.population + 1
+        print("Name:" + name)
+        self.name = name
+
+    def dupName(self):
+        self.name = "{0} - {0}".format(self.name)
+        print(self.name)
+
+    def whoseName(self):
+        return self.name
+
+    def howMany(self):
+        return self.population
+
+
+if __name__ == "__main__":
+    bb = BobClass("Fred")
+    bb.dupName()
+    print("whosename ->")
+    nm = bb.whoseName()
+    print(nm)
+    bb = BobClass("george")
+    bb = BobClass("Sam")
+    print(bb.howMany())
+    counter = bb.howMany()
+    print("Population is {0}".format(counter))
