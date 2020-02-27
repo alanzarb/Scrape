26b1d0d799341b26c5aa6c51d345786a05877b76
Alan HP <alan@zarblet.comm>
2020-02-27 13:39:17 -0500

    n5

--- logzero-File.py
+++ logzero-File.py
@@ -3,13 +3,17 @@ from logzero import logger
 
 # Setup rotating logfile with 3 rotations, each with a maximum filesize of 1MB:
 """was 1e6, not just 90"""
-logzero.logfile("tmp\\rotating-logfile.log", maxBytes=10000, backupCount=3)    
+logzero.logfile("tmp\\rotating-logfile.log", maxBytes=10000, backupCount=3)
 
 # Log messages
-for n in range(12):
+for n in range(1):
     #logger.setLevel("DEBUG")
-    logger.debug(f"Number {n} This log message goes to the console and the logfile")
-    logger.info(f"Number {n} This log message goes to the console and the logfile")
-    logger.warn(f"Number {n} This log message goes to the console and the logfile")
-    logger.error(f"Number {n} This log message goes to the console and the logfile")
-    # logger.exception(f"Number {n} This log message goes to the console and the logfile")
+    logger.debug(
+        f"This log message goes to the console and the logfile")
+    logger.info(
+        f"This log message goes to the console and the logfile")
+    logger.warn(
+        f"This log message goes to the console and the logfile")
+    logger.error(
+        f"This log message goes to the console and the logfile")
+    # logger.exception(f"This log message goes to the console and the logfile")
