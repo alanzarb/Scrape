17060dd809cc9a17b57c0c5e8bc56fa90ee67307
Zarb <alanzarb@users.noreply.github.com>
2020-02-28 21:59:19 -0500

    n9

---
+++ kwargs.py
@@ -0,0 +1,7 @@
+def greet_me(**kwargs):
+    if kwargs is not None:
+        for key, value in kwargs.values
+            print ("Key: {key}  Value: {value} ")
+
+
+greet_me (aa="AAA", bb="BBBB", cc= "CCCC")
