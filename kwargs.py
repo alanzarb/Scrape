25e7d1f5f15bb45f90964069e5c7ff4771b1001e
Zarb <alanzarb@users.noreply.github.com>
2020-02-28 23:52:48 -0500

    n9

--- kwargs.py
+++ kwargs.py
@@ -1,7 +1,22 @@
+""" Pass **kwargs to a function.
+    Iterate through the args
+    """
+
+
 def greet_me(**kwargs):
-    if kwargs is not None:
-        for key, value in kwargs.values
-            print ("Key: {key}  Value: {value} ")
+    # print(kwargs)
+    print(len(kwargs))
+    # if kwargs is not None:
+    print(f"kwargs len is  {len(kwargs.items())}")
+    for key, value in kwargs.items():
+        print(f"Key: {key}  \tValue: {value} ")
 
 
-greet_me (aa="AAA", bb="BBBB", cc= "CCCC")
+greet_me(aa="AAA",
+         bb="BBBB",
+         cc="CCCC",
+         bob="Robert",
+         k456="Fourhundred",
+         m23="MMM23")
+
+greet_me()
